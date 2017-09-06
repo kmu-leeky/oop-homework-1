37ce62f14c799375136bce629a6b42cb67a30204
Kyungyong Lee <leeky@kookmin.ac.kr>
2017-09-06 15:31:18 +0900

    adding skeleton code

---
+++ OOPHomework-1/src/BankInterest.java
@@ -0,0 +1,6 @@
+
+public class BankInterest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ OOPHomework-1/src/SizeCalculator.java
@@ -0,0 +1,6 @@
+
+public class SizeCalculator {
+    public static void main(String[] args) {
+
+    }
+}
